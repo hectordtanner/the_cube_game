8d3ab34a45024bf7af4cd2224d7c3e0b55c60363
hectordtanner <hectordtanner@gmail.com>
2026-07-01 12:26:22 +1200

    began work on tutorial textboxes

--- the_cube_game/Assets/Scripts/TutorialController.cs
+++ the_cube_game/Assets/Scripts/TutorialController.cs
@@ -1,48 +1,72 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.InputSystem;
 
 public class TutorialController : MonoBehaviour
 {
     [SerializeField]
-    private int startX = -100;
-    [SerializeField]
-    private int startY = -100;
-    private Vector2 startPos;
+    private TextMeshProUGUI dialogue;
 
     [SerializeField]
-    private int endX = -180;
-    [SerializeField]
-    private int endY = -180;
-    private Vector2 endPos;
+    private string[] lines;
 
-    private float cycle = 1.0f;
     [SerializeField]
-    private float speed = 0.01f;
+    private float scrollSpeed;
+
+    private int index;
+
+    [SerializeField]
+    private float smoothTime;
+
+    private Vector2 velocity = Vector2.zero;
 
     private RectTransform rectTransform;
 
     void Start()
     {
-        startPos = new Vector2(startX, startY);
-        endPos = new Vector2(endX, endY);
-
+        dialogue.text = string.Empty;
+        StartDialogue();
         rectTransform = GetComponent<RectTransform>();
     }
 
     void Update()
     {
-        if (rectTransform != null)
+        if (Mouse.current.rightButton.isPressed && Mouse.current.delta.ReadValue().magnitude != 0.0f && dialogue.text == lines[index])
         {
-            rectTransform.anchoredPosition = new Vector2((cycle * startX) + ((1.0f - cycle)* endX), (cycle * startY) + ((1.0f - cycle)* endY));
-            cycle -= speed;
-            if ((cycle < 0.0f)||(cycle > 1.0f))
-            {
-                speed *= -1;
-                cycle = Mathf.Clamp(cycle, 0.0f, 1.0f);
-            }
+            NextTutorial(new Vector2(100, 100));
         }
-        else
+    }
+    
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index])
         {
-            Debug.Log("rect is null");
+            dialogue.text += c;
+            yield return new WaitForSeconds(scrollSpeed);
+        }
+    }
+
+    void NextTutorial(Vector2 endPos)
+    {   
+        MoveBox(endPos);
+        gameObject.SetActive(false);
+        index++;
+        dialogue.text = string.Empty;
+    }
+
+    void MoveBox(Vector2 endPos)
+    {
+        while (rectTransform.anchoredPosition != endPos)
+        {
+            rectTransform.anchoredPosition = Vector2.SmoothDamp(rectTransform.anchoredPosition, endPos, ref velocity, smoothTime);
         }
     }
 }
